f8bf6f585d2b8ea28475fc489566597229c81c55
Jack Chou <jackchou@berkeley.edu>
2015-03-09 23:26:28 -0700

    Dynamically generated secret token

---
+++ jortal-app/config/initializers/secret_token.rb
@@ -0,0 +1,16 @@
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Jortal::Application.config.secret_token = secure_token
