179ffc0eb93c458ba91f274c1408de515791927a
Albert Hu <alberthu@berkeley.edu>
2015-03-12 21:55:29 -0700

    fix weird has_context capybara thingy failing

--- features/step_definitions/student_submission_steps.rb
+++ features/step_definitions/student_submission_steps.rb
@@ -42,7 +42,7 @@ end
 
 Then /^(?:|I )should see "([^"]*)"$/ do |text|
   if page.respond_to? :should
-    page.should have_context(text)
+    page.should have_content(text)
   else
     assert page.has_content?(text)
   end
