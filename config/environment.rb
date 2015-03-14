1d2df2a3c274cf0e9265638943b15a346c8e5ba5
Arlan Jaska <ajaska@berkeley.edu>
2015-03-13 21:19:39 -0700

    Add authcas requires

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Jortal::Application.initialize!
 
-# require 'casclient'
-# require 'casclient/frameworks/rails/filter'
+require 'casclient'
+require 'casclient/frameworks/rails/filter'
 
 CASClient::Frameworks::Rails::Filter.configure(
   :cas_base_url => "https://auth.berkeley.edu/cas"
