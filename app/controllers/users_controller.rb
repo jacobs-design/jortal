9d7e347ae9d710b57a9ea014042969b2b9f0b3ee
Jack Chou <jackchou@berkeley.edu>
2015-03-16 22:36:25 -0700

    Refactor rest of users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,15 +39,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    respond_with @user
+#    respond_to do |format|
+#      if @user.save
+#        format.html { redirect_to @user, notice: 'User was successfully created.' }
+#        format.json { render json: @user, status: :created, location: @user }
+#      else
+#        format.html { render action: "new" }
+#        format.json { render json: @user.errors, status: :unprocessable_entity }
+#      end
+#    end
   end
 
   # PUT /users/1
@@ -55,15 +56,16 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
 
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    respond_with @user
+#    respond_to do |format|
+#      if @user.update_attributes(params[:user])
+#        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+#        format.json { head :no_content }
+#      else
+#        format.html { render action: "edit" }
+#        format.json { render json: @user.errors, status: :unprocessable_entity }
+#      end
+#    end
   end
 
   # DELETE /users/1
@@ -72,9 +74,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user.destroy
 
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
-    end
+    respond_with @user
+#    respond_to do |format|
+#      format.html { redirect_to users_url }
+#      format.json { head :no_content }
+#    end
   end
 end
