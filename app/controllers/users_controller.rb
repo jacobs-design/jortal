eb1c95bc5dcf39c486da5befd83cd01f80f972ef
Arlan Jaska <ajaska@berkeley.edu>
2015-04-02 17:08:12 -0700

    Clean up comments from users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
   respond_to :html, :json
 
   # GET /users
-  # GET /users.json
   def index
     @users = User.all
     @who = session[:cas_user]
@@ -14,7 +13,6 @@ class UsersController < ApplicationController
   end
 
   # GET /users/1
-  # GET /users/1.json
   def show
     @user = User.find(params[:id])
 
@@ -22,7 +20,6 @@ class UsersController < ApplicationController
   end
 
   # GET /users/new
-  # GET /users/new.json
   def new
     @user = User.new
 
@@ -35,25 +32,14 @@ class UsersController < ApplicationController
   end
 
   # POST /users
-  # POST /users.json
   def create
     @user = User.new(params[:user])
 
     flash[:notice] = 'User was successfully created.' if @user.save
     respond_with @user
-#    respond_to do |format|
-#      if @user.save
-#        format.html { redirect_to @user, notice: 'User was successfully created.' }
-#        format.json { render json: @user, status: :created, location: @user }
-#      else
-#        format.html { render action: "new" }
-#        format.json { render json: @user.errors, status: :unprocessable_entity }
-#      end
-#    end
   end
 
   # PUT /users/1
-  # PUT /users/1.json
   def update
     @user = User.find(params[:id])
 
@@ -61,27 +47,13 @@ class UsersController < ApplicationController
       flash[:notice] = 'User was successfully updated.'
     end
     respond_with @user
-#    respond_to do |format|
-#      if @user.update_attributes(params[:user])
-#        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-#        format.json { head :no_content }
-#      else
-#        format.html { render action: "edit" }
-#        format.json { render json: @user.errors, status: :unprocessable_entity }
-#      end
-#    end
   end
 
   # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user = User.find(params[:id])
     @user.destroy
 
     respond_with @user
-#    respond_to do |format|
-#      format.html { redirect_to users_url }
-#      format.json { head :no_content }
-#    end
   end
 end
