c964ef8a28e76c89304334a9fbe188bd5428ca48
Arlan Jaska <ajaska@berkeley.edu>
2015-04-15 13:38:22 -0700

    Actually redirect to downloads

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -105,9 +105,9 @@ class ProjectsController < ApplicationController
   end
 
   def download
-    @file = AttachmentUploader.new
-    @file.retrieve_from_store!('test.txt')
-    redirect_to project_path(id: params[:id], file: @file) # figure out path?!
+    file_accessor = AttachmentUploader.new
+    file_accessor.retrieve_from_store!('test.txt')
+    redirect_to file_accessor.url
   end
 
 end
