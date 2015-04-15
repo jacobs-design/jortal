3c85ad61af4cdf1599cbc63a9dac55af6f6750ca
Arlan Jaska <ajaska@berkeley.edu>
2015-04-15 16:52:59 -0700

    Add projects and users relation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
   attr_accessible :desc, :name
   has_many :submissions
+  has_and_belongs_to_many :users
   validates :desc, :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :admin, :email, :name, :uid
+  has_and_belongs_to_many :projects
   validates :email, presence: true
   validates :name, presence: true
   validates :uid, presence: true, uniqueness: true
