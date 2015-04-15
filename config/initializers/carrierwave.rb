4cb1399b97474c4291c87cb2db24529e7a2843e3
Albert Hu <alberthu@berkeley.edu>
2015-04-14 22:37:54 -0700

    fix typo in instructor_download feature and change carrierwave config to use local filesystem in testing environment

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,16 @@
 CarrierWave.configure do |config|
-    config.storage = :fog
-    config.fog_credentials = {
-      provider:                'AWS',
-      aws_access_key_id:       ENV['AWS_ACCESS_KEY'], 
-      aws_secret_access_key:   ENV['AWS_SECRET_KEY'],
-      endpoint:                'https://s3.amazonaws.com',
-      region:                  'us-east-1'
-    }
-    config.fog_directory = ENV['AWS_BUCKET'] 
+    if Rails.env.test?
+        config.storage = :file
+        config.enable_processing = false
+    else 
+        config.storage = :fog
+        config.fog_credentials = {
+          provider:                'AWS',
+          aws_access_key_id:       ENV['AWS_ACCESS_KEY'], 
+          aws_secret_access_key:   ENV['AWS_SECRET_KEY'],
+          endpoint:                'https://s3.amazonaws.com',
+          region:                  'us-east-1'
+        }
+        config.fog_directory = ENV['AWS_BUCKET'] 
+    end
 end
