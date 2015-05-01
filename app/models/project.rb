ba729c92a2c52bc6432b367a42d9b15e0a7057b5
Jack Chou <jackchou@berkeley.edu>
2015-05-01 03:01:56 -0300

    Delete submissions upon deletion of parent project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
   attr_accessible :desc, :name
-  has_many :submissions
+  has_many :submissions, dependent: :destroy
   has_and_belongs_to_many :users
   validates :desc, :name, presence: true
 end
