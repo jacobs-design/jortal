1c2be5607941b8468f397aecccf812f3bf63a067
Mina Li <li.mina2007@gmail.com>
2015-05-02 01:00:14 -0300

    Instructors did not have the ability to create a project before. Now they do.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   before_filter CASClient::Frameworks::Rails::Filter
-  after_filter :verify_authorized, :except => [:submit_submission]
+  after_filter :verify_authorized, :except => [:submit_submission, :new, :create]
   respond_to :html, :json
 
   rescue_from Pundit::NotAuthorizedError, ActiveRecord::RecordNotFound, with: :redirect_to_submit_submission
@@ -25,21 +25,27 @@ class ProjectsController < ApplicationController
 
   # GET /projects/new
   def new
-    @project = Project.new
-    @project.users = [current_user]
-    authorize @project
+    if is_user?
+      @project = Project.new
+      @project.users = [current_user]
 
-    respond_with @project
+      respond_with @project
+    else
+      redirect_to_submit_submission
+    end
   end
 
   # POST /projects
   def create
-    @project = Project.new(params[:project])
-    @project.users = [current_user]
-    authorize @project
+    if is_user?
+      @project = Project.new(params[:project])
+      @project.users = [current_user]
 
-    flash[:notice] = 'Project was successfully created.' if @project.save
-    respond_with @project
+      flash[:notice] = 'Project was successfully created.' if @project.save
+      respond_with @project
+    else
+      redirect_to_submit_submission
+    end
   end
 
   # DELETE /projects/1
